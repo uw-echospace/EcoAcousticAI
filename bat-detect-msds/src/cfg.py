ffd8799dcf3e590fa6a9eba414aa3fc8987a38e3
Isha Gokhale <igokhale101@gmail.com>
2025-02-27 18:13:04 -0800

    uupdate file path

--- bat-detect-msds/src/cfg.py
+++ bat-detect-msds/src/cfg.py
@@ -62,28 +62,28 @@ def get_config():
         # Input audio is divided into segments of this duration (seconds), each processed individually
         "segment_duration": 30.0, 
         
-        "models": [ 
-            BatCallDetector( 
+        "models": [
+            BatCallDetector(
                 # TODO: all parameters 
                 detection_threshold=0.5,
                 spec_slices=False,
                 chunk_size=2, 
                 #model_path=f"{os.path.dirname(__file__)}/models/bat_call_detector/batdetect2/models/Net2DFast_UK_same.pth.tar",
-                model_path="./models/bat_call_detector/batdetect2/models/Net2DFast_UK_same.pth.tar"
+                model_path="./models/bat_call_detector/batdetect2/models/Net2DFast_UK_same.pth.tar",
                 time_expansion_factor=1.0,
                 quiet=False,
                 cnn_features=True,
                 peak_distance=0.05,
                 peak_threshold=0.25,
                 #template_dict_path=f"{os.path.dirname(__file__)}/models/bat_call_detector/templates/template_dict.pickle",
-                template_dict_path="./models/bat_call_detector/templates/template_dict.pickle"
+                template_dict_path="./models/bat_call_detector/templates/template_dict.pickle",
                 num_matches_threshold=2,
                 buzz_feed_range=0.15,
                 alpha=1,
-            ),
+                ),
         #    AnotherDetector(
         #         param1="param1",
         #         param2="param2",  
         #     )
         ]
-    } 
+    }
