2a5dd9b938e8fc0f68435721f1303ac7e17eb35d
Isha Gokhale <igokhale101@gmail.com>
2025-03-16 00:07:12 -0700

    run on week data

--- run_frognet.sh
+++ run_frognet.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Loop through each directory in the array
-while IFS= read -r directory; do
-  echo "Running Docker on directory:" $directory
+# # Loop through each directory in the array
+# while IFS= read -r directory; do
+#   echo "Running Docker on directory:" $directory
 
-  if [ -z "$(ls -A "$directory")" ]; then
-      echo "Skipping empty directory."
-      continue
-  fi
+#   if [ -z "$(ls -A "$directory")" ]; then
+#       echo "Skipping empty directory."
+#       continue
+#   fi
 
-  # Run Docker for each directory
-    docker run --rm \
-        --mount type=bind,source=$directory,target=/app/frog_audio/ \
+#   # Run Docker for each directory
+#     docker run --rm \
+#         --mount type=bind,source=$directory,target=/app/frog_audio/ \
+#         --mount type=bind,source=/mnt/ecoacoustic-storage/,target=/app/results/frogs/ \
+#         frognet:latest
+
+# done < new_directories.txt
+
+ docker run --rm \
+        --mount type=bind,source=/tmp/osn_bucket/,target=/app/frog_audio/ \
         --mount type=bind,source=/mnt/ecoacoustic-storage/,target=/app/results/frogs/ \
         frognet:latest
-
-done < new_directories.txt
