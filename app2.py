747569e2821220883e35d070bd3a8e22a15755ea
Lawrie Brunswick <85261391+lawrieb@users.noreply.github.com>
2025-03-17 00:56:47 -0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -400,7 +400,7 @@ elif page == "models":
         col1, col2 = st.columns([1, 5])
         
         with col1:
-            st.image(model['logo'], width=70)
+            st.image(model['logo'], width=400)
             
         with col2:
             st.markdown(f"### [{model['name']}]({model['url']})")
