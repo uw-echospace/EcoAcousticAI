2fd7f21920a50261858a23fac09c6018eb0b1b6c
Isha Gokhale <igokhale101@gmail.com>
2025-03-16 17:03:21 -0700

    run birdnet

--- birdnetlib/run_both_models.sh
+++ birdnetlib/run_both_models.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Run the first script
-python3 /app/predict_frog_species.py
+python3 /app/predict_birdnet_all.py
+
 
 # Run the second script
-python3 /app/predict_birdnet_all.py
+python3 /app/predict_frog_species.py
